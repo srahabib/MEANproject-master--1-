0cd21a1bb2e84888a23bc0d27ff52b42cb571ac2
sra <sara.habib48@gmail.com>
2022-10-10 23:28:52 +0200

    login/logout works

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'search/:searchTerm', component:HomeComponent},
-  {path:'tag/:tag',component:HomeComponent},
-  {path:'food/:id',component:FoodPageComponent},
-  {path:'cart-page' , component: CartPageComponent},
-  {path:'login',component:LoginPageComponent}
+  { path: '', component: HomeComponent },
+  { path: 'search/:searchTerm', component: HomeComponent },
+  { path: 'tag/:tag', component: HomeComponent },
+  {path:'food/:id', component:FoodPageComponent},
+  {path:'cart-page', component: CartPageComponent},
+  {path:'login', component: LoginPageComponent}
 ];
 
 @NgModule({
